3e840afe50d91bb74fe6d4d51b2d6081550c7fed
alireza <alireza.karamii1997@gmail.com>
2025-07-08 11:26:29 +0330

    added navbar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const closeBtn = document.getElementById("closeBanner");
 const banner = document.getElementById("banner");
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+
 
 main();
 function main(){
 closeBtn.addEventListener("click", () => {
-  banner.style.display = "none";
+    banner.style.display = "none";
+});
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
 });
 }
 
