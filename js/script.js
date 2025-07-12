512173d5d3c41ba3becc57e5b6f7b55aaf62c605
alireza <alireza.karamii1997@gmail.com>
2025-07-12 15:42:41 +0330

    added timer js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 const closeBtn = document.getElementById("closeBanner");
 const banner = document.getElementById("banner");
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+//offer timer
+const hoursBox =document.getElementById("hours");
+const minutesBox =document.getElementById("minutes");
+const secondsBox =document.getElementById("seconds");
+
+const endTime= new Date();
+endTime.setHours(23,59,59,1000);
+
+
 
 
 main();
@@ -13,4 +22,31 @@ closeBtn.addEventListener("click", () => {
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
+
+
 }
+// timer function
+function updateTime(){
+    const now=new Date();
+    const diff = endTime - now;
+
+    if(diff <= 0){
+        hoursBox.textContent ="00";
+        minutesBox.textContent ="00";
+        secondsBox.textContent ="00";
+        clearInterval(countdown);
+        return;
+    }
+
+    const hours = Math.floor(diff/(1000*60*60));
+    const minutes = Math.floor((diff%(1000*60*60)) / (1000*60));
+    const seconds = Math.floor((diff%(1000*60)) / 1000);
+   
+    hoursBox.textContent = hours < 10 ? "0" + hours : hours;
+    minutesBox.textContent = minutes < 10 ? "0" + minutes : minutes;
+    secondsBox.textContent = seconds < 10 ? "0" + seconds : seconds;
+
+
+} 
+const countdown = setInterval(updateTime , 1000);
+updateTime();
